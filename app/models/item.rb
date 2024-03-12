2fd944465a7089db3fca41d549fe1d23ff9d721a
Gambizzz <anelisegambini@yahoo.fr>
2024-03-12 10:29:44 +0100

    modifs 1st day

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
-    validates :title,presence: true
-    validates :description,presence: true
+    validates :title, presence: true
+    validates :description, presence: true
     validates :price, numericality: { greater_than: 0, less_than: 1000 }, presence: true
     validates :image_url, presence: true
 
