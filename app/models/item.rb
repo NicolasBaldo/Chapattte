6ad040cf7b2d861a382af64fddfe84484bac2194
yeezynico <yeezy77680@gmail.com>
2024-03-12 16:50:58 +0100

    test

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than: 0, less_than: 1000 }, presence: true
     validates :image_url, presence: true
 
+    has_many :carts
+    has_many :orders
 end
 
