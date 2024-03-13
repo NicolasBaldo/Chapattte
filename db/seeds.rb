c14e1df3b0837857188b61e6123ffb8147c850e1
yeezynico <yeezy77680@gmail.com>
2024-03-13 09:41:07 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,24 +4,19 @@ Item.destroy_all
 User.destroy_all
 
 search_terms = ['cat']
-<<<<<<< HEAD
-20.times do
-  new_item = Item.create(title: Faker::Creature::Cat.breed, description: Faker::ChuckNorris.fact, price: Faker::Commerce.price, image_url: Faker::LoremFlickr.image)
-end
-=======
+
 6.times do
   new_item = Item.create(title: Faker::Creature::Cat.breed, description: Faker::ChuckNorris.fact, price: Faker::Commerce.price, image_url: Faker::LoremFlickr.image(search_terms: [search_terms.cycle.next]))
 end
 
-20.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = "#{first_name}.#{last_name}@yopmail.com"
+# 20.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = "#{first_name}.#{last_name}@yopmail.com"
 
-  User.create(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    password: 'password')
-  end
->>>>>>> devlopment
+#   User.create(
+#     first_name: first_name,
+#     last_name: last_name,
+#     email: email,
+#     password: 'password')
+#   end
