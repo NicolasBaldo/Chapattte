99f4ca97bd8d13cafbcc5d8bfcbe8ec72d353d39
NicolasBaldo <septfoisept@outlook.fr>
2024-03-13 20:58:38 +0100

    all links done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
-    def team
+        def team
         end
-    def uglycats
-           end
-    def fatcats
+        def uglycats
         end
-        def desiguesedcats
+        def fatcats
         end
-end
+            def desiguesedcats
+        end
+        def contact
+        end
+        def help 
+        end 
+        def privacypolicy
+        end
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,15 @@ Rails.application.routes.draw do
   resources :carts
   resources :users
 
-  # Route vers la page d'équipe (assumant que vous avez un contrôleur StaticPagesController)
   get 'team', to: 'static_pages#team'
   get 'uglycats', to: 'static_pages#uglycats'
   get 'fatcats', to:  'static_pages#fatcats'
   get 'desiguesedcats', to: 'static_pages#desiguesedcats'
-  # Décommentez cette ligne si vous avez besoin de définir une route vers la page d'équipe du contrôleur ItemsController
-  # get 'team', to: 'items#index' 
+  get 'contact', to: 'static_pages#contact'
+  get 'help', to: 'static_pages#help'
+  get 'privacypolicy', to: 'static_pages#privacypolicy'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
 end
