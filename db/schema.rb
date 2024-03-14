3cca2fa7923efb8bf6c36ef42aaa96916506104a
NicolasBaldo <septfoisept@outlook.fr>
2024-03-14 10:27:57 +0100

    schema heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_123004) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_212411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_123004) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "join_table_items_carts", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "cart_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_join_table_items_carts_on_cart_id"
+    t.index ["item_id"], name: "index_join_table_items_carts_on_item_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "stripe_customer_id"
     t.bigint "user_id"
@@ -68,4 +78,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_123004) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "items"
   add_foreign_key "carts", "users"
+  add_foreign_key "join_table_items_carts", "carts"
+  add_foreign_key "join_table_items_carts", "items"
 end
