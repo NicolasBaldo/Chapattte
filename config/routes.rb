c81235f347d4904a808d7359adc3e4178d6b493e
yeezynico <yeezy77680@gmail.com>
2024-03-14 23:22:27 +0100

    stipefinii

--- config/routes.rb
+++ config/routes.rb
@@ -10,20 +10,20 @@ Rails.application.routes.draw do
   resources :orders
   resources :cart_items, only: [:create, :destroy]
 
-  
+
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
     get 'success', to: 'checkout#success', as: 'checkout_success'
     get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
 end
 
-  # get 'team', to: 'static_pages#team'
-  # get 'uglycats', to: 'static_pages#uglycats'
-  # get 'fatcats', to:  'static_pages#fatcats'
-  # get 'desiguesedcats', to: 'static_pages#desiguesedcats'
-  # get 'contact', to: 'static_pages#contact'
-  # get 'help', to: 'static_pages#help'
-  # get 'privacypolicy', to: 'static_pages#privacypolicy'
+  get 'team', to: 'static_pages#team'
+  get 'uglycats', to: 'static_pages#uglycats'
+  get 'fatcats', to:  'static_pages#fatcats'
+  get 'desiguesedcats', to: 'static_pages#desiguesedcats'
+  get 'contact', to: 'static_pages#contact'
+  get 'help', to: 'static_pages#help'
+  get 'privacypolicy', to: 'static_pages#privacypolicy'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
