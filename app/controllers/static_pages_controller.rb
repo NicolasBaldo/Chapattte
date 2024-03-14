13027efe3f81ee014bdb32d5b3d125c2eefa5df7
NicolasBaldo <septfoisept@outlook.fr>
2024-03-14 11:58:22 +0100

    il manque plus que deux pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,13 @@ class StaticPagesController < ApplicationController
         def team
         end
         def uglycats
+            @items = Item.all
         end
         def fatcats
+            @items = Item.all
         end
             def desiguesedcats
+            @items = Item.all
         end
         def contact
         end
